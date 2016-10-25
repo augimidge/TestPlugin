90818e38ec34e9322b67fe8dee585adf3f8d10de
augimidge <zacjaccard92@gmail.com>
2016-10-25 12:32:48 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      
+      app: {
+        title: 'Augi Testplugin',
+        description: 'firstplugin',
+        main: 'plugins/augi_testplugin/app'
+      },
+      
+      
+      hacks: [
+        'plugins/augi_testplugin/hack'
+      ]
+      
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+    
+
+  });
+};
